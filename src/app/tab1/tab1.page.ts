a93cbabef3532e9b66e5111b4e450f61d409e564
Jonathan Rojas <jonathansw01gmail.com>
2021-02-16 01:04:16 -0300

    Primer commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  	selector: 'app-tab1',
+  	templateUrl: 'tab1.page.html',
+  	styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
 
-  constructor() {}
 
+
+export class Tab1Page implements OnInit {
+
+
+	constructor() {}
+
+
+	
+	ngOnInit() {
+
+		// Depurar código (la app se detendrá en este punto)
+		//debugger;
+
+		console.log('Hola Mundo');
+	
+	}
+
+	  
 }
